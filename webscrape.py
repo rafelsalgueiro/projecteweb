b9d2d85b0ab4398d4836f671888922eb1759669b
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-02-20 09:25:59 +0100

    adding libraries

--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,23 @@
+import urllib3
 
+def webScrape(object):
 
+    def __init__(self)-> None:
+        pass
 
+    def get_web(self):
+        #conectar http
+
+        #enviar peticio
+        #guardar html resposta
+        pass
+
+    def get_data(self):
+        # descarregar web
+        self.get_web()
+        # llegir dades
+        # return dades
+        pass
 
 if __name__ =="__main__":
     client = webScrape()
